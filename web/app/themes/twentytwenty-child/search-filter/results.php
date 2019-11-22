c9f01e22790816ef7c3465e40445c9569b10bc1b
Richard Kim <rkim00@users.noreply.github.com>
2019-11-21 19:57:10 -0500

    Add files via upload

---
+++ web/app/themes/twentytwenty-child/search-filter/results.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Results Template for Water for Grassroots to work with the Search & Filter Pro plug-in
+ * Adapted from the Search & Filter sample Results template
+ * 
+ * @package   Search_Filter
+ * @author    Ross Morsali
+ * @link      https://searchandfilter.com
+ * @copyright 2018 Search & Filter
+ * 
+ * Note: these templates are not full page templates, rather 
+ * just an encaspulation of the your results loop which should
+ * be inserted in to other pages by using a shortcode - think 
+ * of it as a template part
+ * 
+ * This template is an absolute base example showing you what
+ * you can do, for more customisation see the WordPress docs 
+ * and using template tags - 
+ * 
+ * http://codex.wordpress.org/Template_Tags
+ *
+ */
+
+if ( $query->have_posts() )
+{
+	?>
+    <p class="num-results"><?php echo $query->found_posts; ?> organizations</p>
+	<?php
+	while ($query->have_posts())
+	{
+		$query->the_post();
+		
+		?>
+		<div>
+			<h3"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+			<p class="results-excerpt"><?php the_excerpt(); ?></p>
+		</div>
+		
+		<hr />
+		<?php
+	}
+	?>
+	<p class="page-of-page">Page <?php echo $query->query['paged']; ?> of <?php echo $query->max_num_pages; ?></p>
+	
+	<div class="pagination">
+		
+		<span class="nav-previous"><?php previous_posts_link( '&lt;' ); ?></span>
+		<span class="nav-next"><?php next_posts_link( '&gt;', $query->max_num_pages ); ?></span>
+		<?php
+			/* example code for using the wp_pagenavi plugin */
+			if (function_exists('wp_pagenavi'))
+			{
+				echo "<br />";
+				wp_pagenavi( array( 'query' => $query ) );
+			}
+		?>
+	</div>
+	<?php
+}
+else
+{
+	echo '<p class="no-results">No Results Found</p>';
+}
+?>
